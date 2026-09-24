8cea90d8e29857b22c5ac332b47a5aa319b318ea
Danil Silantyev <danil@nddev.it.com>
2026-09-24 23:51:30 +0500

    feat: a release of this repository
    Published at 0.0.74. Propose changes through this repository's issues and
    pull requests.

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ set -eu
 
 REPO="NDDev-OpenNetwork/pi-setup-system"
 BINARY="pi-setup-system"
-VERSION="${1:-0.0.73}"
+VERSION="${1:-0.0.74}"
 PREFIX="${PI_INSTALL_DIR:-$HOME/.local/bin}"
 
 case "$(uname -s)" in
